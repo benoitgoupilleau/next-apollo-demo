52559b2df02ce9bc04b6f485ced56838cf00f072
Benoit Goupilleau <benoit.goupilleau@gmail.com>
2021-07-04 15:07:31 +0200

    Added docker

--- client/lib/appolo-client.ts
+++ client/lib/appolo-client.ts
@@ -1,10 +1,24 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, InMemoryCache, HttpLink, from } from '@apollo/client';
 import { offsetLimitPagination } from '@apollo/client/utilities';
 
+import { onError } from '@apollo/client/link/error';
+
 const uri = process.env.NEXT_PUBLIC_GRAPHQL_URI;
+const httpLink = new HttpLink({
+  uri,
+});
+
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors)
+    graphQLErrors.forEach(({ message, locations, path }) =>
+      console.log(`[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`)
+    );
+
+  if (networkError) console.log(`[Network error]: ${networkError}`);
+});
 
 const client = new ApolloClient({
-  uri,
+  link: from([errorLink, httpLink]),
   cache: new InMemoryCache({
     typePolicies: {
       Query: {
--- server/server.ts
+++ server/server.ts
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 apolloServer.applyMiddleware({ app });
 
-const port = process.env.PORT || 5000;
+const port = process.env.SERVER_PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Graphql Server started on: http://localhost:${port}`);
